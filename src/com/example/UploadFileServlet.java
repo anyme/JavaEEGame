2cfae67dd29050b59002931bcca57658c1f83051
Anastasia Kryukova <nihility1155@gmail.com>
2015-03-08 16:52:34 +0100

    Clone InputStream and save in cache

--- src/com/example/UploadFileServlet.java
+++ src/com/example/UploadFileServlet.java
@@ -1,13 +1,13 @@
 package com.example;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
@@ -25,13 +25,10 @@ public class UploadFileServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String filename;
 
-        ServletInputStream inputStream = request.getInputStream();
-
-        if (inputStream != null) {
-                InputStream in = request.getPart("file").getInputStream();
                 if ((filename = getFileName(request.getPart("file"))) != null) {
                     FileCache.cache.put(filename, in);
                 }
+        InputStream in = new BufferedInputStream(request.getPart("file").getInputStream());
         }
     }
 
